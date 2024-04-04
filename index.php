b1e11ab9e1db3f951b49219aec825f2f5cb8e360
Александр Осипян <aleksandrosipan@Aleksandrs-MacBook-Air.local>
2024-04-04 15:17:25 +0300

    php files autorisation

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    $title = "Main Page";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,18 +10,29 @@
 </head>
 <body>
     <form action="index.php" method="post">
-        <label>price:</label><br>
-        <input type="text" name="price"><br>
-        <label>quantitiy:</label><br>
-        <input type="text" name="quantitiy"><br>
-        <input type="submit" value="total">
+        <label>E-mail:</label><br>
+        <input type="text" name="login"><br>
+        <label>Password:</label><br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Log in">
     </form>
+    <a href="https://youtu.be/PFRGWuMPx4E?si=d-Lvj_InAnyP7l9-">Don't have an account?</a>
 </body>
 </html>
 
 <?php
-    $price = $_POST["price"];
-    $quantitiy = $_POST["quantitiy"];
-    $q = $_POST["price"] * $_POST["quantitiy"];
-    echo "total: {$price} * {$quantitiy} = {$q}";
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+    $flag = 0;
+    if(strlen($password) < 8){
+        echo "<br>" . "Password is not valid";
+        $flag++;
+    } 
+    if(!filter_var($login, FILTER_VALIDATE_EMAIL)){
+        echo "<br>" . "E-mail is not valid";
+        $flag++;
+    }
+    if($flag > 0){
+        echo "<br>" . "Try again!";
+    }
 ?>
