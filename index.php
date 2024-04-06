a4396be64275b39ad9c633d20100eeea3186e84e
Александр Осипян <aleksandrosipan@Aleksandrs-MacBook-Air.local>
2024-04-06 17:21:55 +0300

    making token

--- index.php
+++ index.php
@@ -1,21 +1,45 @@
 <?php
     $title = "Main Page";
 ?>
+<!-- css style -->
+<style type="text/css" media="all">
+@import url("index.css");
+</style>
+<!--  -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    	<!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+
+<!--  -->
+<svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="40" cy="40" r="40" fill="white" />
+  <circle cx="40" cy="25.1163" r="14.6977" fill="#DADADA" />
+  <mask id="mask0_1_19" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="80" height="80">
+    <circle cx="40" cy="40" r="40" fill="#D9D9D9" />
+  </mask>
+  <g mask="url(#mask0_1_19)">
+    <circle cx="40" cy="68.6512" r="26.2326" fill="#DADADA" />
+  </g>
+</svg>
+<!--  -->
+
     <h1>Log in</h1>
     <form action="auth_check.php" method="post">
         <label>E-mail:</label><br>
         <input type="email" name="email" id="email" required><br><br>
         <label>Password:</label><br>
         <input type="password" name="password" id="password" required><br><br>
-        <input type="submit" value="Log in">
+        <button type="submit"><b>Log in</b></button>
     </form>
     <br>
     <a href="reg/registration.php">Don't have an account?</a>
--- profile/profile.php
+++ profile/profile.php
@@ -1,11 +1,31 @@
+<!-- <?php
+
+//welcome.php
+// require 'vendor/autoload.php';
+
+// use Firebase\JWT\JWT;
+// use Firebase\JWT\Key;
+
+// $key = '1a3LM3W966D6QTJ5BJb9opunkUcw_d09NCOIJb9QZTsrneqOICoMoeYUDcd_NfaQyR787PAH98Vhue5g938jdkiyIZyJICytKlbjNBtebaHljIR6-zf3A2h3uy6pCtUFl1UhXWnV6madujY4_3SyUViRwBUOP-UudUL4wnJnKYUGDKsiZePPzBGrF4_gxJMRwF9lIWyUCHSh-PRGfvT7s1mu4-5ByYlFvGDQraP4ZiG5bC1TAKO_CnPyd1hrpdzBzNW4SfjqGKmz7IvLAHmRD-2AMQHpTU-hN2vwoA-iQxwQhfnqjM0nnwtZ0urE6HjKl6GWQW-KLnhtfw5n_84IRQ';
+
+// if(isset($_COOKIE['token'])){
+// 	$decoded = JWT::decode($_COOKIE['token'], new Key($key, 'HS256'));
+// } else {
+// 	header('location:index.php');
+// }
+
+?> -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    
     <title>Document</title>
 </head>
 <body>
-    <h1>profile page</h1>
+    <h1>Profile page</h1>
+    <a href="../logout.php">Logout</
 </body>
 </html>
